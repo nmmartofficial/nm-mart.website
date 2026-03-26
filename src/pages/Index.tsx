4a5ae0dc0b0d2ae73cd98a0b3d335aa3fb4e84ce
nmmartofficial <nmmart07@gmail.com>
2026-03-26 18:57:56 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,15 +2,20 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import ProductGrid from "@/components/ProductGrid";
 import WelfareCard from "@/components/WelfareCard";
+import ProductSearch from "@/components/ProductSearch"; // यह नई लाइन है
 import Reviews from "@/components/Reviews";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import BackToTop from "@/components/BackToTop";
 
 const Index = () => (
-  <div className="min-h-screen flex flex-col">
+  <div className="min-h-screen flex flex-col bg-[#000814]">
     <Header />
     <Hero />
+    
+    {/* 5000 Products Search Section */}
+    <ProductSearch /> 
+
     <ProductGrid />
     <WelfareCard />
     <Reviews />
