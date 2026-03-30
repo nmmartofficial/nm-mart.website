ca2e08c626b338cf6fa61b8caf0144ee422f0c6a
nmmartofficial <nmmart07@gmail.com>
2026-03-30 14:39:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ProductDetail from "./pages/ProductDetail";
 import Login from "./pages/Login";
-import About from "./pages/About";
+import About from "./pages/Abbout";
 import Contact from "./pages/Contact";
 import Privacy from "./pages/Privacy";
 import NotFound from "./pages/NotFound";
