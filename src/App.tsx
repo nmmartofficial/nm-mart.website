410c434ce04ca9a1c59371f2a9d9f2757fbc830b
nmmartofficial <nmmart07@gmail.com>
2026-03-30 12:45:16 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
             <Route path="/" element={<Index />} />
             <Route path="/product/:slug" element={<ProductDetail />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/about" element={<Aboutt />} />
+            <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<Privacy />} />
             <Route path="*" element={<NotFound />} />
